316c38839c2bb613bf9f383410a04c61fd28351c
Sascha <sascha.fluor@gmx.net>
2019-09-25 12:04:41 +0200

    Korrektur

--- Test.java
+++ Test.java
@@ -2,13 +2,14 @@ public class Test {
 	public static void main (String [] args){
 
 
-		short a = 1;
+		short a = 2;
 		short b = 2;
 		short c = (short) (a + b);
 
 		System.out.println (a);
 		System.out.println (b);
 		System.out.println (c);
+
 
 
 	}	
