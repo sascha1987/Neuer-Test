f44cd3c50b36d0cbb5b257e4504fb3e9ec7a180e
Sascha <sascha.fluor@gmx.net>
2019-09-25 14:19:52 +0200

    Anpassung

--- Test.java
+++ Test.java
@@ -5,12 +5,14 @@ public class Test {
 		short a = 2;
 		short b = 2;
 		short c = (short) (a + b);
+		short d = 5;
+
+
 
 		System.out.println (a);
 		System.out.println (b);
 		System.out.println (c);
-
-
+		System.out.println (d);
 
 	}	
 }
