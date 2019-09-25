65a9fd6d2c8d3cf1ffe02eb3b655e34c0535eac8
Sascha <sascha.fluor@gmx.net>
2019-09-25 12:00:58 +0200

    Neues File!

---
+++ Test.java
@@ -0,0 +1,15 @@
+public class Test {
+	public static void main (String [] args){
+
+
+		short a = 1;
+		short b = 2;
+		short c = (short) (a + b);
+
+		System.out.println (a);
+		System.out.println (b);
+		System.out.println (c);
+
+
+	}	
+}
